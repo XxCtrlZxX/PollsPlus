c64c0aab03c020dda274577905ca2addbde4fd0d
eunwoosong <songe00348@gmail.com>
2020-12-07 21:40:43 +0900

    Update user

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.template import loader
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 
+
+def signup(request):
+    if request.method == "POST":
+        userid = request.POST.get("userid", None)
+        username = request.POST.get("username", None)
+        password1 = request.POST.get("password1", None)
+        password2 = request.POST.get("password2", None)
+
+        if userid is None:
+            return render(request, "accounts/signup.html", {"error": "input id"})
+
+        if username is None:
+            return render(request, "accounts/signup.html", {"error": "input name"})
+
+        if password1 is None:
+            return render(request, "accounts/signup.html", {"error": "input pw"})
+
+        if password2 is None:
+            return render(request, "accounts/signup.html", {"error": "input confirm pw "})
+
+        if password1 == password2:
+            user = User.objects.create_user(userid=userid, password=password1, username=username)
+            user.save()
+            auth.login(request, user)
+            return render('accounts/index.html', {"userid": userid})
+    return render(request, 'accounts/signup.html')
+
+
+def signin(request):
+    if request.method == "POST":
+        userid = request.POST["userid"]
+        password = request.POST["password"]
+        if userid is None:
+            return render(request, "accounts/signin.html", {"error": "input id"})
+
+        if password is None:
+            return render(request, "accounts/signin.html", {"error": "input pw"})
+
+        user = auth.authenticate(request, userid=userid, password=password)
+        if user is None:
+            return render(request, 'accounts/signin.html', {"error": "username or password is incorrect"})
+        else:
+            auth.login(request, user)
+            return render('accounts/index.html', {"userid": userid})
+
+
+def signout(request):
+    auth.logout(request)
+    return render(request, "accounts/index.html")
+
+
+def index(request):
+    render(request, 'accounts/index.html')
 # Create your views here.
--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('accounts/', include('accounts.urls'))
 ]
 
 app_name = 'polls'
